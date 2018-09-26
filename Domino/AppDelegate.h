0ea6ec8e1ae6eb03afa66fdfb0d5f41d8ba04dc7
gy_lyons <gy_lyons@163.com>
2018-09-26 09:38:38 +0800

    no message

---
+++ Domino/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Domino
+//
+//  Created by lyons on 2018/9/26.
+//  Copyright © 2018 lyons. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Domino/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Domino
+//
+//  Created by lyons on 2018/9/26.
+//  Copyright © 2018 lyons. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
